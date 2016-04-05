959c9c1448cea0412ff2c8fc236fb5f5f93436c3
Alexander Metelchenko <alxmetel@gmail.com>
2016-04-05 14:17:05 +0300

    Initial project version. Make the main page design, connect to database

---
+++ README.md
@@ -0,0 +1,20 @@
+This is the web-application "Online Library".
+
+It will have this functionality:
+
+- user authorization;
+- data editing;
+- login and password check;
+- data adding;
+- multi-language support;
+- books search by title and author;
+- online book view;
+- books downloading;
+- session saving;
+... and I will continue and edit tis list step-by-step.
+
+Creating this application I am going to use these technologies and tools:
+JSF, Hibernate, AJAX, Facelets, PrimeFaces, OmniFaces, MySQL and others.
+
+
+I will update this file step-by-step to show, what has already been used.
---
+++ src/java/com/metel/web/beans/AuthorList.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.metel.web.beans;
+
+import com.metel.web.db.Database;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Metel
+ */
+public class AuthorList {
+    
+    private ArrayList<Author> authorList = new ArrayList<>();
+    
+    private ArrayList<Author> getAuthors() {
+               
+        Statement stmt = null;
+        ResultSet rs = null;
+        Connection conn = null;
+        
+        try {
+        conn = Database.getConnection();
+        stmt = conn.createStatement();
+        rs = stmt.executeQuery("select * from author");
+        while (rs.next()) {
+            Author author = new Author();
+            author.setName(rs.getString("fio"));
+            authorList.add(author);
+        }
+        } catch (SQLException ex) {
+            Logger.getLogger(AuthorList.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                if (stmt!=null) stmt.close();
+                if (rs!=null)rs.close();
+                if (conn!=null)conn.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(AuthorList.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return authorList;
+    }
+    
+    public ArrayList<Author> getAuthorList() {
+        if (!authorList.isEmpty()) {
+            return authorList;
+        } else {
+            return getAuthors();
+        }
+    }
+}
